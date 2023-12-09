4a4e5f4473052b09bf1a2d74204ee62e8efefafb
Dymek-Kamil <kamil.dymek33@gmail.com>
2023-12-09 12:22:29 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/faq-accorin-main-Frontendmnetor",
   plugins: [react()],
+  base: "/faq-accorin-main-Frontendmnetor/",
 });
